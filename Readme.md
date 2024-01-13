badd15e40d77ef2fbd841c33f854257138767c87
Hemanth Kumar S <hemanthkumars0499@gmail.com>
2024-01-13 23:07:08 +0530

    First commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# The React Quiz
+
+## This project is a quiz app where I have used react js, focusing on useReducer hook,
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main className="main">
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;

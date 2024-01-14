8b98ca7350383fe1f9953cdebd0abc8b036e1e4e
Hemanth Kumar S <123663912+hemanthmanu04@users.noreply.github.com>
2024-01-14 16:45:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -101,7 +101,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("https://the-react-quiz-ashy.vercel.app/questions")
+    fetch("https://the-react-quiz-zeta.vercel.app/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
